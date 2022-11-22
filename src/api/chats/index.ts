77c16671babf6c163854e70ced9c3267f16c937d
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-11-22 16:05:49 +0000

    added note

--- src/api/chats/index.ts
+++ src/api/chats/index.ts
@@ -6,7 +6,7 @@ const chatsRouter = express.Router();
 
 // INITIAL CHATS REQUEST (check if chat exists)
 
-
+// We will receive the members of the chat in this request, use them with .populate to check if there is already a chat with these people in the users chats array
 
 // GET all chats I'm a part of
 
