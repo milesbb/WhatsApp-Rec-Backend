982da60fd4d86ca0d40f6a19dda52800b116b81d
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-11-22 15:00:21 +0000

    added test plan

--- src/__tests__/users.test.ts
+++ src/__tests__/users.test.ts
@@ -1,29 +1,51 @@
-import supertest from "supertest"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import server from "../server"
+import supertest from "supertest";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import server from "../server";
 
+dotenv.config();
 
-dotenv.config()
+const client = supertest(server);
 
-const client = supertest(server)
-
-
-beforeAll(async () => {
-   
+describe("Test User Endpoints", async () => {
+  beforeAll(async () => {
     if (process.env.MONGO_TEST_CONNECTION) {
-      await mongoose.connect(process.env.MONGO_TEST_CONNECTION)
+      await mongoose.connect(process.env.MONGO_TEST_CONNECTION);
     } else {
-      throw new Error("Mongo URL missing!")
+      throw new Error("Mongo URL missing!");
     }
-  })
+  });
 
+  // Check MongoDB connection is not undefined
+
+  // Register User Bad Data (POST)
+
+  // Register User (POST)
+
+  // Login User (POST)
+
+  // Refresh Tokens (POST)
+
+  // GET all users
+
+  // GET me
+
+  // PUT me
+
+  // GET user specific
+
+  // GET user specific non-existent ID
+
+  // POST user avatar
+
+  // Logout (DELETE)
+
+  // Test Logout
+
+
+  // After All Close Connection
 
   afterAll(async () => {
-    
-    await mongoose.connection.close()
-  })
-
-
-    
-
+    await mongoose.connection.close();
+  });
+});
