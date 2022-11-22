2796a1b4ec697d92763bf54b407a4486a8692b39
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-11-22 14:51:56 +0000

    added chat endpoint shells

--- src/api/chats/index.ts
+++ src/api/chats/index.ts
@@ -1,5 +1,33 @@
 import express from "express";
+import { JwtAuthenticationMiddleware } from "../../lib/jwtAuth";
 
 const chatsRouter = express.Router();
 
-export default chatsRouter
+// GET all chats I'm a part of
+
+chatsRouter.get("/", JwtAuthenticationMiddleware, async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Create a chat with other participant(s)
+
+chatsRouter.post("/", JwtAuthenticationMiddleware, async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET full message history for specific chat
+
+chatsRouter.get("/:id", JwtAuthenticationMiddleware, async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default chatsRouter;
